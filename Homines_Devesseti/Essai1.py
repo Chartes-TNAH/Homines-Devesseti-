699b0dfef6be5d3bacf8139ebe81526401e5e262
root <root@LAPTOP-8EEGAU04.localdomain>
2022-01-15 12:33:35 +0100

    Site moche mais un peu près fonctionnel à partir du cours sur Flask

--- Homines_Devesseti/Essai1.py
+++ Homines_Devesseti/Essai1.py
@@ -2,13 +2,31 @@ from flask import Flask, render_template
 
 app = Flask("Homines Devesseti")
 
-@app.route("/")
-@app.route("/exemple/<exemple>")
-def accueil(exemple=None):
-    lieux = ["Devesset", "Saint-Agrève"]
-    if exemple:
-        lieux = ["Col. Lugdunum", "Augustomagus"]
-    return render_template("accueil.html", nom="Gazetteer", lieux=lieux)
+hommes = {
+    0: {
+        "nom": "Garralis",
+        "prenom": "Andrea",
+        "localite": "Devesset",
+        "fonction": "notaire",
+        "date": "20 juin 1322"
+    },
+    1: {
+        "nom": "Verilhati",
+        "prenom": "Jean",
+        "fonction": "curé",
+        "date": "12 septembre 1323",
+    }
+}
 
+
+@app.route("/")
+def accueil():
+    return render_template("pages/accueil.html", nom="Homines Devesseti", hommes=hommes)
+
+@app.route("/name/<int:name_id>")
+def nom(name_id):
+    return render_template("pages/noms.html", nom="Homines Devesseti", homme=hommes[name_id])
+
+# Petit tips final pour s'assurer que Python s'est pas perdu en chemin et exécute bien ce fichier
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
