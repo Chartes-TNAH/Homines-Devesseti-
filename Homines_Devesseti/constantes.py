03e76031423e1abe71a1baa58008437cdbdd378b
root <root@LAPTOP-8EEGAU04.localdomain>
2022-03-03 16:59:03 +0100

    encore des modifs

--- Homines_Devesseti/constantes.py
+++ Homines_Devesseti/constantes.py
@@ -6,3 +6,18 @@ API_ROUTE = "/api"
 
 if SECRET_KEY == "JE SUIS UN SECRET !":
     warn("Le secret par défaut n'a pas été changé, vous devriez le faire", Warning)
+
+#COnfiguration des bases
+class _TEST:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_db.sqlite'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class _PRODUCTION:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///homines_devesseti.db?check_same_thread=False'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+CONFIG = {
+    "test": _TEST,
+    "production": _PRODUCTION
+}
